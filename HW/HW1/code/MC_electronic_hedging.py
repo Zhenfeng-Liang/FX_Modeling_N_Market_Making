acb011f50e12d98e9149e36782949b8d75b70120
Jose <joseleungbit@gmail.com>
2015-09-05 13:38:44 -0400

    Changed to generate vectorized pnl

--- HW/HW1/code/MC_electronic_hedging.py
+++ HW/HW1/code/MC_electronic_hedging.py
@@ -81,8 +81,78 @@ def MC_trigger(num_paths):
     return
 
 
+def simulate_one_path():
+    ## This is for experiment to get a vector of PNL
+
+    S0 = 1
+    sigma = 0.1 # per year
+    _lambda_ = 1 # trade per second
+    trade_size = 1
+    prob_long_trade = 0.5
+    client_trade_spread = 0.0001
+    hedge_trade_spread = 0.0002
+    
+    delta_limit = 0.5  # units, assuming it is positive 
+    hedge_target = 0
+
+    dt_sec = 0.1 / _lambda_  # seconds
+    prob_trade = 1 - exp(-_lambda_ * dt_sec)
+    time_steps = 3
+
+    trading_days_per_year = 260
+    dt = dt_sec / (trading_days_per_year * 24 * 60 * 60)
+
+    sigma_dt = sqrt(dt) * sigma
+    
+    dS = sigma_dt * np.random.randn(time_steps)
+
+    
+    # Cumsum each row, e.g each simulation path
+    S = np.append(1, dS).cumsum()
+    
+    trade_size_mat = np.where( np.random.rand(time_steps + 1) < prob_trade, trade_size, 0 )
+    long_short_mat = np.where( np.random.rand(time_steps + 1) < prob_long_trade, 1, -1 )
+    trade_size_mat = trade_size_mat * long_short_mat
+    del long_short_mat
+    
+    #received_pnl = S.dot(np.absolute(trade_size_mat)) * client_trade_spread * 0.5
+    # Vector elements multiplication
+    pnl_vec = S * np.absolute(trade_size_mat) * client_trade_spread * 0.5
+    print "Received pnl is ", pnl_vec
+    
+    
+    pos = np.zeros(time_steps + 1)
+    
+    #paid_pnl = 0
+    for i in range(time_steps + 1):
+        # Note the little trick here. I get pos[i-1] is the last element of the pos which is zero. This makes the formula neater.
+        pos[i] = pos[i-1] + trade_size_mat[i]
+        
+        if fabs(pos[i]) > fabs(delta_limit):
+            print "Before hedged: ", pos[i]
+
+            hedge_notional = fabs(pos[i]) - fabs(hedge_target)
+            pnl_vec[i] = hedge_trade_spread * S[i] * hedge_notional * 0.5
+            if pos[i] > 0:
+                pos[i] = hedge_target
+            else:
+                pos[i] = -1 * hedge_target
+                
+            print "After hedged: ", pos[i]
+        
+    print "pnl including hedging pnl", pnl_vec
+
+    pnl_vec[1:len(pnl_vec)] += pos[:len(pos)-1] * dS
+    
+    print "Total PNL is ", pnl_vec
+
+    
+    return
+
+
 def main():
-    MC_trigger(1)
+    
+    simulate_one_path()
 
     return
 
