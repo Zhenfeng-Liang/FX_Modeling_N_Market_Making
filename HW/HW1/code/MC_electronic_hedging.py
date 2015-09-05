777e25eb590e6f35795b6ebee57545bee34fb73f
Jose <joseleungbit@gmail.com>
2015-09-04 20:26:48 -0400

    Initial commit for the code part

---
+++ HW/HW1/code/MC_electronic_hedging.py
@@ -0,0 +1,8 @@
+'''
+Copyright (c) 2015 Zhenfeng Liang, Baruch College 
+Created on Sep 1, 2015
+@author: Zhenfeng Liang
+@contact: zhenfeng.jose.liang@gmail.com
+@description: main function to run a monte carlo to simulate electronic hedging 
+@usage: 
+'''
